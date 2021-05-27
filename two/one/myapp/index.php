e0ead56c92453b22e201395209a9fda85a284952
u0113 <francisco.mancardi@tesisquare.com>
2021-05-27 22:51:14 +0200

    code examples

---
+++ two/one/myapp/index.php
@@ -0,0 +1,22 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$pieces = ["two","one","myapp"];
+$piecesQty = count($pieces);
+
+$vap = str_repeat("/.." , $piecesQty);
+require __DIR__ . $vap . '/vendor/autoload.php';
+
+define('BASEPATH',"/" . implode("/",$pieces));
+$app = AppFactory::create();
+$app->setBasePath(BASEPATH);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+            $ohh = "Hello world! >> " . BASEPATH;
+            $response->getBody()->write($ohh);
+            return $response;
+});
+
+$app->run(); 
